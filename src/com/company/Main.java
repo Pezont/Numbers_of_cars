b4a5510f031331c7d8cd490c0c9d07b51ca2ed71
Pezont <anton11sherh@gmail.com>
2024-03-23 19:36:00 +0300

    add readme

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+//import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int n;
+//        String d = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+//        //char c = d.charAt( n = (int) (Math.random()+35));
+//
+//        for (int p =0; p<10; p++){for(int i =0; i<12; i++){
+//                char c = d.charAt( n = (int) (1+Math.random()*35));
+//                System.out.print(c);
+//                //
+//            }System.out.println();}
+
+
+        
+
+        int XXxx;
+        int xxXX;
+        int sum=0;
+       for (int i = 1000; i < 9999; i++) {
+            XXxx = i / 100;
+            xxXX = i % 100;
+            if (XXxx+xxXX==100) {
+                sum++;
+                System.out.println(i);
+            }
+        }
+//        System.out.println(sum);
+//       for(int n =0; n<45;n++){
+//           if(n%2==0)
+//       }
+    }
+}
